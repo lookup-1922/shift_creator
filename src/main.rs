90e26d4cd7068b9dd7ac13b0a621c389aba8137f
lookup-1922 <125456965+lookup-1922@users.noreply.github.com>
2024-05-26 13:12:38 +0900

    起動時の選択を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use dialoguer::{Input, Select};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Searching files...");
+
+    loop {
+        println!("実行したい操作を教えて下さい。");
+        let menu = &["シフト作成", "設定"];
+        let choice = Select::new().items(menu).default(0).interact().unwrap();
+
+        match choice {
+            0 => {
+                println!("シフト作成ですね。");
+                break;
+            }
+            1 =>{
+                println!("設定ですね。");
+                break;
+            }
+            _ => {
+                println!("エラー");
+            }
+        }
+    }
 }
